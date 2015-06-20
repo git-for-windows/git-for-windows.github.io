e491b75095c8aa380bcc74a1c26ac451d596aacc
paladox <paladox@users.noreply.github.com>
2015-06-20 11:39:10 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
 			compress: {
 				files: {
 					'js/pack.js': [
-						'js/jquery-1.10.2.min.js',
+						'js/jquery-1.11.3.min.js',
 						'js/jquery.fancybox.js',
 						'js/jquery.mousewheel.js'
 					]
